b091559877869cd980c13d6e39af752d5c252e3e
David B ZHANG <18588744489@163.com>
2023-05-24 20:45:56 +0800

    Update

--- app/src/main/java/com/wwe/MainActivity.kt
+++ app/src/main/java/com/wwe/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.wwe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val asyncExecuteHandler: Handler by lazy {
+        val worker = HandlerThread("asyncExecuteWorker")
+        worker.start()
+        return@lazy Handler(worker.looper)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /**
+     * 判断下当前线程，如果是主线程则调度到一个专门负责创建线程的线程进行工作。
+     */
+//    fun execute(runnable: Runnable, priority: Int) {
+//        if (Looper.getMainLooper().thread == Thread.currentThread() && asyncExecute) {
+//            //异步执行
+//            asyncExecuteHandler.post {
+//                mExecutor.execute(runnable, priority)
+//            }
+//        } else {
+//            mExecutor.execute(runnable, priority)
+//        }
+//    }
 }
